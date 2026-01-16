6cef0074b253403de34733881cb0c3e18366df09
marcos <marcoslopezmarinn@gmail.com>
2026-01-16 13:27:03 +0100

    refactor application configuration; update API gateway routes and CORS settings; enhance global request filter for traceability; modify user ID generation strategy

--- users-service/src/main/java/com/marcos/usersservice/entity/User.java
+++ users-service/src/main/java/com/marcos/usersservice/entity/User.java
@@ -3,6 +3,7 @@ package com.marcos.usersservice.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
@@ -22,7 +23,7 @@ import lombok.Setter;
 })
 public class User extends AbstractAuditableEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private String username;
