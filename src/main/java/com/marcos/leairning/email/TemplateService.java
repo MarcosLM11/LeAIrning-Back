9d2d3284adebb78708b30a73dca46d5606b784a2
Marcos <marcoslopezmarinn@gmail.com>
2026-09-23 18:45:11 +0200

    email service fix.

--- src/main/java/com/marcos/leairning/email/TemplateService.java
+++ src/main/java/com/marcos/leairning/email/TemplateService.java
@@ -1,28 +1,27 @@
 package com.marcos.leairning.email;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class TemplateService {
-
     private final TemplateEngine templateEngine;
 
-    public TemplateService(TemplateEngine templateEngine) {
-        this.templateEngine = templateEngine;
-    }
-
     public String render(String templateName, Map<String, Object> variables) {
-        var context = new Context();
-        context.setVariables(variables);
-        return templateEngine.process(templateName, context);
+        return process(templateName, variables);
     }
 
     public String renderText(String templateName, Map<String, Object> variables) {
+        return process(templateName + "-text", variables);
+    }
+
+    private String process(String name, Map<String, Object> variables) {
         var context = new Context();
         context.setVariables(variables);
-        return templateEngine.process(templateName + "-text", context);
+        return templateEngine.process(name, context);
     }
 }
